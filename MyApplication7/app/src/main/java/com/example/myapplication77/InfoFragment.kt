ec296b9ca312294afb0c494e93d40d2ada6016e8
Diablo <banetrw@gmail.com>
2023-12-13 23:56:23 +0100

    Created fragments, bottom menu and almost completed infoFragment

---
+++ MyApplication7/app/src/main/java/com/example/myapplication77/InfoFragment.kt
@@ -0,0 +1,92 @@
+package com.pma.apolloapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.myapplication77.R
+
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [InfoFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class InfoFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private lateinit var img_srb: ImageView
+    private lateinit var img_eng: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+
+
+        return inflater.inflate(R.layout.fragment_info, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment InfoFragment.
+         */
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            InfoFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var txtView : TextView = view.findViewById(R.id.text)
+
+        var str_srb = resources.getString(R.string.string_srb)
+
+
+        var str_eng = resources.getString(R.string.string_eng)
+
+        img_srb = view.findViewById(R.id.img_srb)
+        img_eng = view.findViewById(R.id.img_usa)
+
+
+        img_srb.setOnClickListener {
+
+            txtView.text = str_srb
+
+        }
+
+        img_eng.setOnClickListener {
+            txtView.text = str_eng
+        }
+
+
+    }
+}
--- MyApplication7/app/src/main/java/com/example/myapplication77/MainActivity.kt
+++ MyApplication7/app/src/main/java/com/example/myapplication77/MainActivity.kt
@@ -1,14 +1,55 @@
 package com.example.myapplication77
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.myapplication77.databinding.ActivityMainBinding
+import com.pma.apolloapp.InfoFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    //this is how fragments are changed
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+
+        fragmentTransaction.commit()
+
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //Skida status bar
         getSupportActionBar()?.hide()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when (it.itemId) {
+
+                R.id.home -> replaceFragment(HomeFragment())
+
+                R.id.profile -> replaceFragment(InfoFragment())
+
+                R.id.logout -> {
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+
+                else ->{}
+            }
+            true
+
+        }
     }
 }
